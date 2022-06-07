656aea74f109d11f972559aada7cfa0a8b950d71
Fadi Abuaita <fadimabuaita@gmail.com>
2022-06-07 13:06:10 +0300

    first jsx assignment

---
+++ First JSX/first-react-jsx/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import List from './components/List'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <List/>
+    </div>
+  );
+}
+
+export default App;
---
+++ First JSX/first-react-jsx/src/components/List.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+
+class List extends Component{
+
+    render(){
+        return(
+            <>
+                <h2>Things I Need To Do:</h2>
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a Marathon</li>
+                <li>Feed the Dogs</li>
+            </>
+        )
+    }
+
+}
+export default List;
